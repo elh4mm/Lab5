5ed9f136267777834a31c16ad3b5ae5dff68e9df
Elham Miandej <emiandej@my.centennialcollege.ca>
2020-04-29 16:04:01 -0400

    Add project files.

---
+++ Exercise1/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblfactorial = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtFactorial = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtValues = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtHighIndex = new System.Windows.Forms.TextBox();
+            this.txtLowIndex = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.rbInteger = new System.Windows.Forms.RadioButton();
+            this.rbDouble = new System.Windows.Forms.RadioButton();
+            this.rbChar = new System.Windows.Forms.RadioButton();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get Factorial of:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblfactorial);
+            this.groupBox1.Controls.Add(this.btnCalc);
+            this.groupBox1.Controls.Add(this.txtFactorial);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 192);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculate Asynchronously";
+            // 
+            // lblfactorial
+            // 
+            this.lblfactorial.AutoSize = true;
+            this.lblfactorial.Location = new System.Drawing.Point(45, 150);
+            this.lblfactorial.Name = "lblfactorial";
+            this.lblfactorial.Size = new System.Drawing.Size(0, 20);
+            this.lblfactorial.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(23, 88);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(125, 41);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtFactorial
+            // 
+            this.txtFactorial.Location = new System.Drawing.Point(135, 37);
+            this.txtFactorial.Name = "txtFactorial";
+            this.txtFactorial.Size = new System.Drawing.Size(100, 26);
+            this.txtFactorial.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblNum);
+            this.groupBox2.Controls.Add(this.btnCheck);
+            this.groupBox2.Controls.Add(this.txtNumber);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(249, 200);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Check even or odd";
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(19, 86);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(0, 20);
+            this.lblNum.TabIndex = 3;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(23, 119);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(196, 44);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check for Even and Odd";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(127, 48);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(108, 26);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Input Number:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtValues);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.txtOut);
+            this.groupBox3.Controls.Add(this.btnDisplay);
+            this.groupBox3.Controls.Add(this.btnSearch);
+            this.groupBox3.Controls.Add(this.txtHighIndex);
+            this.groupBox3.Controls.Add(this.txtLowIndex);
+            this.groupBox3.Controls.Add(this.txtSearch);
+            this.groupBox3.Controls.Add(this.rbInteger);
+            this.groupBox3.Controls.Add(this.rbDouble);
+            this.groupBox3.Controls.Add(this.rbChar);
+            this.groupBox3.Controls.Add(this.btnGenerate);
+            this.groupBox3.Location = new System.Drawing.Point(267, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(601, 409);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Display a list of value and shearch";
+            // 
+            // txtValues
+            // 
+            this.txtValues.Location = new System.Drawing.Point(25, 84);
+            this.txtValues.Multiline = true;
+            this.txtValues.Name = "txtValues";
+            this.txtValues.Size = new System.Drawing.Size(84, 299);
+            this.txtValues.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(124, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(282, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Output of values between low and high";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Input high Index";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Input low Index";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Input Value for search";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(127, 318);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(456, 65);
+            this.txtOut.TabIndex = 10;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(465, 189);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(99, 41);
+            this.btnDisplay.TabIndex = 9;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(465, 103);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(99, 43);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtHighIndex
+            // 
+            this.txtHighIndex.Location = new System.Drawing.Point(302, 228);
+            this.txtHighIndex.Name = "txtHighIndex";
+            this.txtHighIndex.Size = new System.Drawing.Size(100, 26);
+            this.txtHighIndex.TabIndex = 7;
+            // 
+            // txtLowIndex
+            // 
+            this.txtLowIndex.Location = new System.Drawing.Point(302, 166);
+            this.txtLowIndex.Name = "txtLowIndex";
+            this.txtLowIndex.Size = new System.Drawing.Size(100, 26);
+            this.txtLowIndex.TabIndex = 6;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(302, 103);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 26);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // rbInteger
+            // 
+            this.rbInteger.AutoSize = true;
+            this.rbInteger.Location = new System.Drawing.Point(25, 31);
+            this.rbInteger.Name = "rbInteger";
+            this.rbInteger.Size = new System.Drawing.Size(85, 24);
+            this.rbInteger.TabIndex = 4;
+            this.rbInteger.TabStop = true;
+            this.rbInteger.Text = "Integer";
+            this.rbInteger.UseVisualStyleBackColor = true;
+            // 
+            // rbDouble
+            // 
+            this.rbDouble.AutoSize = true;
+            this.rbDouble.Location = new System.Drawing.Point(170, 31);
+            this.rbDouble.Name = "rbDouble";
+            this.rbDouble.Size = new System.Drawing.Size(85, 24);
+            this.rbDouble.TabIndex = 3;
+            this.rbDouble.TabStop = true;
+            this.rbDouble.Text = "Double";
+            this.rbDouble.UseVisualStyleBackColor = true;
+            // 
+            // rbChar
+            // 
+            this.rbChar.AutoSize = true;
+            this.rbChar.Location = new System.Drawing.Point(314, 31);
+            this.rbChar.Name = "rbChar";
+            this.rbChar.Size = new System.Drawing.Size(68, 24);
+            this.rbChar.TabIndex = 2;
+            this.rbChar.TabStop = true;
+            this.rbChar.Text = "Char";
+            this.rbChar.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(402, 25);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(162, 47);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate Values";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 454);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Asynchronous Programming";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtFactorial;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtHighIndex;
+        private System.Windows.Forms.TextBox txtLowIndex;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.RadioButton rbInteger;
+        private System.Windows.Forms.RadioButton rbDouble;
+        private System.Windows.Forms.RadioButton rbChar;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblfactorial;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.TextBox txtValues;
+    }
+}
+
---
+++ Exercise1/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1
+{
+    public partial class Form1 : Form
+    {
+        public delegate bool CheckOddOrEven(int number);
+        StringBuilder builder;
+        int[] intArr;
+        char[] charArr;
+        double[] doubArr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = Convert.ToInt32(txtFactorial.Text);
+                Task<double> task1 = Task.Run(() => GetFactorial(number));
+                lblfactorial.Text = "Calculating...";
+                await task1;
+                lblfactorial.Text = task1.Result.ToString();
+
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Please provide number only");
+            }
+
+        }
+
+        private double GetFactorial(int number)
+
+        {
+
+
+            System.Threading.Thread.Sleep(4000);
+
+            if (number == 1)
+                return 1;
+            else
+                return number * GetFactorial(number - 1);
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            CheckOddOrEven evenPredicate = IsEven;
+            CheckOddOrEven oddPredicate = IsOdd;
+            try
+            {
+                int number = Convert.ToInt32(txtNumber.Text);
+                if (number == 0)
+                    throw new Exception();
+
+                if (evenPredicate(number))
+                {
+                    lblNum.Text = "The number is even";
+                }
+
+                else if (oddPredicate(number))
+                {
+                    lblNum.Text = "The number is odd";
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Please provide number bigger than zero only");
+            }
+
+        }
+
+        public void RandomString()
+        {
+            builder = new StringBuilder();
+            Random random = new Random();
+
+            
+    
+            
+            int i = 0;
+            if (rbDouble.Checked)
+            {
+                doubArr = new double[10];
+                double c;
+                while (i < 10)
+                {
+                    c = random.NextDouble() * (99 - 10) + 10 ;
+                    if (!doubArr.Any(a => a == c))
+                    {
+                        doubArr[i] = Math.Round(c,1);
+                      builder.Append($"{ doubArr[i++]}\r\n");
+
+                    }
+                    
+                }
+            }
+
+            if (rbChar.Checked )
+            {
+               charArr = new char[10];
+                char c;
+              
+               while (i < 10)
+                {
+                    c = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                    if (!charArr.Any(a => a == c))
+                    {
+                        charArr[i] = c;
+                  
+                        builder.Append($"{charArr[i++]}\r\n");
+                    }
+                }
+            }
+
+            if (rbInteger.Checked)
+            {
+                 intArr = new int[10];
+                int c;
+                while (i < 10)
+                {
+                    c = random.Next(10,99);
+
+
+                    if (!intArr.Any(a => a == c))
+                    {
+                        intArr[i] = c;
+                        builder.Append($"{intArr[i++]}\r\n");
+
+                    }
+                }
+            }
+            txtValues.Text = builder.ToString();
+     
+        }
+
+
+        private static bool IsEven(int number) => number % 2 == 0;
+        private static bool IsOdd(int number) => number % 2 == 1;
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            RandomString();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            bool res=false;
+            try
+            {
+
+                if (rbDouble.Checked)
+                    res = Search(doubArr,double.Parse (txtSearch.Text));
+                if (rbChar.Checked)
+                    res = Search(charArr, char.Parse(txtSearch.Text.ToUpper()));
+                if (rbInteger.Checked)
+                    res = Search(intArr, int.Parse(txtSearch.Text));
+                if (res)
+                    MessageBox.Show($"\"{txtSearch.Text}\" is found");
+            }
+            catch (Exception )
+            {
+
+                MessageBox.Show("Input a valid Search value");
+            }
+               
+        }
+
+        private  bool Search<T>(T[] arr , T searchKey) where T : IComparable<T> 
+        {
+            bool result = false;
+            try
+            {
+                if ( searchKey == null)
+                    throw new Exception("Input Search value");
+
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+
+                    if (arr[i].CompareTo(searchKey)==0)
+                    {
+                        result = true;
+                        break;
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                
+            }
+            return result;
+        }
+
+
+        private void Display<T>(T[] arr)
+        { 
+            
+            for (int i = int.Parse(txtLowIndex.Text); i <= int.Parse(txtHighIndex.Text); i++)
+            {
+                txtOut.Text += $"{arr[i]}\t";
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int low = int.Parse(txtLowIndex.Text);
+                int high = int.Parse(txtHighIndex.Text);
+                if (low > high)
+                    throw new Exception("Low index should not be  higher than high index ");
+
+
+                txtOut.Text = "";
+                if (rbDouble.Checked)
+                    Display(doubArr);
+                if (rbChar.Checked)
+                    Display(charArr);
+                if (rbInteger.Checked)
+                    Display(intArr);
+            }
+            catch (Exception ee)
+            {
+
+                MessageBox.Show(ee.Message);
+            }
+           
+        }
+    }
+}
+   
